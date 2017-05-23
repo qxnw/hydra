ee15a1b75e4f6f4d66e62b5f0a7ca2e720bb9d8f
zkfy <lib4net@163.com>
2017-05-23 12:22:37 +0800

    添加go插件示例

---
+++ sample/wx_base_core_api/handlers/hander_wx_notify.go
@@ -0,0 +1,35 @@
+package handlers
+
+import "github.com/qxnw/hydra_plugin/plugins"
+
+type wxNotify struct {
+	mustFields []string
+}
+
+func newWXNotify() *wxNotify {
+	return &wxNotify{
+		mustFields: []string{"appid", "session"},
+	}
+}
+func (n *wxNotify) initParams(ctx plugins.Context, invoker plugins.RPCInvoker) (wxCtx *wxContext, err error) {
+	wxCtx, err = getWXContext(ctx, invoker)
+	if err != nil {
+		return
+	}
+	err = wxCtx.CheckMustFields(n.mustFields...)
+	return
+}
+
+func (n *wxNotify) Handle(service string, ctx plugins.Context, invoker plugins.RPCInvoker) (status int, result string, err error) {
+	status = 500
+	//输入化context,并检查输入参数
+	wxContext, err := n.initParams(ctx, invoker)
+	if err != nil {
+		return
+	}
+	defer wxContext.Close()
+	//业务处理
+
+	//返回结果
+	return 200, "SUCCESS", nil
+}
