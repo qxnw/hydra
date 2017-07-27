3045f831792b62e064dd3cf56207dfd2edf4db02
zkfy <lib4net@163.com>
2017-07-27 16:34:04 +0800

    修改flags

---
+++ hydra/flags.go
@@ -0,0 +1,109 @@
+package hydra
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/qxnw/hydra/engine"
+	"github.com/qxnw/hydra/registry"
+	"github.com/qxnw/hydra/server"
+	"github.com/qxnw/lib4go/net"
+	"github.com/qxnw/lib4go/transform"
+	"github.com/spf13/pflag"
+)
+
+//HFlags hydra输入参数绑定
+type HFlags struct {
+	currentRegistry        string
+	Domain                 string
+	runMode                string
+	tag                    string
+	mask                   string
+	trace                  string
+	IsDebug                bool
+	crossRegistry          string
+	rpcLogger              bool
+	currentRegistryAddress []string
+	crossRegistryAddress   []string
+	ip                     string
+	baseData               *transform.Transform
+}
+
+//BindFlags 绑定参数列表
+func (h *HFlags) BindFlags(flag *pflag.FlagSet) {
+	flag.StringVarP(&h.currentRegistry, "registry center address", "r", "", "注册中心地址(格式：zk://192.168.0.159:2181,192.168.0.158:2181)")
+	flag.StringVarP(&h.mask, "ip mask", "i", "", "ip掩码(本有多个IP时指定，格式:192.168.0)")
+	flag.StringVarP(&h.tag, "server tag", "t", "", "服务器名称(默认为本机IP地址)")
+	flag.StringVarP(&h.trace, "enable trace", "p", "", "启用项目性能跟踪cpu/mem/block/mutex/server")
+	flag.BoolVarP(&server.IsDebug, "enable debug", "d", false, "是否启用调试模式")
+	flag.StringVarP(&h.crossRegistry, "cross  registry  center address", "c", "", "跨域注册中心地址")
+	flag.BoolVarP(&h.rpcLogger, "use rpc logger", "g", false, "使用RPC远程记录日志")
+}
+
+//CheckFlags 检查输入参数
+func (h *HFlags) CheckFlags(i ...int) (err error) {
+	pflag.Parse()
+	index := 1
+	if len(i) > 0 {
+		index = i[0]
+	}
+	if len(os.Args) < index {
+		return errors.New("未指定域名称")
+	}
+	engine.IsDebug = server.IsDebug
+	h.Domain = os.Args[index]
+	if h.currentRegistry == "" {
+		h.runMode = modeStandalone
+		h.currentRegistryAddress = []string{"localhost"}
+		h.currentRegistry = fmt.Sprintf("%s://%s", h.runMode, strings.Join(h.currentRegistryAddress, ","))
+	} else {
+		h.runMode = modeCluster
+		h.runMode, h.currentRegistryAddress, err = registry.ResolveAddress(h.currentRegistry)
+		if err != nil {
+			return fmt.Errorf("集群地址配置有误:%v", err)
+		}
+	}
+	if h.crossRegistry != "" {
+		if strings.Contains(h.crossRegistry, "//") {
+			return fmt.Errorf("跨域注册中心地址不能指定协议信息:%s(err:%v)", h.crossRegistry, err)
+		}
+		h.crossRegistryAddress = strings.Split(h.crossRegistry, ",")
+	}
+	h.ip = net.GetLocalIPAddress(h.mask)
+	if h.tag == "" {
+		h.tag = h.ip
+	}
+	h.baseData = transform.NewMap(map[string]string{
+		"ip": h.ip,
+	})
+	h.tag = h.baseData.Translate(h.tag)
+	return nil
+}
+
+//ToArgs 转换为可执行的参数列表
+func (h *HFlags) ToArgs() []string {
+	args := make([]string, 0, 4)
+	args = append(args, h.Domain)
+	args = append(args, "-r", h.currentRegistry)
+	if h.tag != "" {
+		args = append(args, "-t", h.tag)
+	}
+	if h.trace != "" {
+		args = append(args, "-p", h.trace)
+	}
+	if h.mask != "" {
+		args = append(args, "-i", h.mask)
+	}
+	if h.IsDebug {
+		args = append(args, "-d")
+	}
+	if h.crossRegistry != "" {
+		args = append(args, "-c", h.crossRegistry)
+	}
+	if h.rpcLogger {
+		args = append(args, "-g")
+	}
+	return args
+}
