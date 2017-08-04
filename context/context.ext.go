0cd0b7de483593b102f5c462e2cb64268dbd5915
zkfy <lib4net@163.com>
2017-08-04 15:24:21 +0800

    *

--- context/context.ext.go
+++ context/context.ext.go
@@ -3,6 +3,7 @@ package context
 import (
 	"errors"
 	"fmt"
+	"strconv"
 )
 
 //GetVarParam 获取var参数值，需提供在ext中提供__func_var_get_
@@ -35,8 +36,11 @@ func (c *Context) CheckArgs(names ...string) error {
 }
 
 //GetArgValue 获取arg.value值
-func (c *Context) GetArgValue(name string) string {
+func (c *Context) GetArgValue(name string, d ...string) string {
 	v, _ := c.GetArgByName(name)
+	if v == "" && len(d) > 0 {
+		return d[0]
+	}
 	return v
 }
 
@@ -48,6 +52,34 @@ func (c *Context) GetArgByName(name string) (string, error) {
 		return "", fmt.Errorf("args配置错误，缺少:%s参数:%v", name, c.GetArgs())
 	}
 	return db, nil
+}
+
+//GetArgIntValue 从args中获取int数字
+func (c *Context) GetArgIntValue(name string) (int, error) {
+	value, err := c.GetArgByName(name)
+	if err != nil {
+		return 0, err
+	}
+	v, err := strconv.Atoi(value)
+	if err != nil {
+		err = fmt.Errorf("arg.%s的值不是有效的int值", name)
+		return 0, err
+	}
+	return v, nil
+}
+
+//GetArgFloat64Value 从args中获取float64数字
+func (c *Context) GetArgFloat64Value(name string) (float64, error) {
+	value, err := c.GetArgByName(name)
+	if err != nil {
+		return 0, err
+	}
+	v, err := strconv.ParseFloat(value, 64)
+	if err != nil {
+		err = fmt.Errorf("arg.%s的值不是有效的float值", name)
+		return 0, err
+	}
+	return v, nil
 }
 
 //GetVarParamByArgsName 根据args参数名获取var参数的值
