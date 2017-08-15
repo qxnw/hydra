9dd4f717ae1e5efdac9eac52076c62d468601c08
zkfy <lib4net@163.com>
2017-08-15 15:09:54 +0800

    重构response

---
+++ context/response.i.go
@@ -0,0 +1,43 @@
+package context
+
+import "github.com/qxnw/lib4go/types"
+
+type ObjectReponse struct {
+	Content interface{}
+	*baseResponse
+}
+
+func GetObjectReponse() *ObjectReponse {
+	return &ObjectReponse{baseResponse: &baseResponse{Params: make(map[string]interface{})}}
+}
+
+func (r *ObjectReponse) GetContent() interface{} {
+	return r.Content
+}
+
+func (r *ObjectReponse) Success(v ...interface{}) *ObjectReponse {
+	r.Status = 200
+	if len(v) > 0 {
+		r.Content = v[0]
+		return r
+	}
+	r.Content = "SUCCESS"
+	return r
+}
+func (r *ObjectReponse) SetContent(status int, content string) *ObjectReponse {
+	r.Status = types.DecodeInt(status, 0, 200, status)
+	r.Content = content
+	return r
+}
+
+func (r *ObjectReponse) Set(s int, rr interface{}, p map[string]string, err error) error {
+	r.Status = s
+	if r.Status == 0 {
+		r.Status = types.DecodeInt(err, nil, 500, 200)
+	}
+	for k, v := range p {
+		r.Params[k] = v
+	}
+	r.Content = rr
+	return err
+}
