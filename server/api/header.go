a522399a19abf47a46eaedc77c1fe3e09fada523
zkfy <lib4net@163.com>
2017-08-24 17:49:07 +0800

    优化服务器启动

---
+++ server/api/header.go
@@ -0,0 +1,25 @@
+package api
+
+import "strings"
+
+func WriteHeader() HandlerFunc {
+	return func(ctx *Context) {
+		if len(ctx.Server.Headers) > 0 {
+			for k, v := range ctx.Server.Headers {
+				if k == "Access-Control-Allow-Origin" {
+					if strings.Contains(v, ctx.req.Host) {
+						hosts := strings.Split(v, ",")
+						for _, h := range hosts {
+							if strings.Contains(h, ctx.req.Host) && strings.Contains(h, ctx.req.Proto) {
+								ctx.Header().Set(k, v)
+								continue
+							}
+						}
+					}
+				}
+				ctx.Header().Set(k, v)
+			}
+		}
+		ctx.Next()
+	}
+}
