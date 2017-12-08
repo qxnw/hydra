b79b367147e84f6a4f8072d2adceb72952a19958
zkfy <lib4net@163.com>
2017-12-08 13:54:59 +0800

    .

---
+++ hydra/trace.go
@@ -0,0 +1,26 @@
+package hydra
+
+import (
+	"net/http"
+	"os"
+	"runtime/trace"
+
+	"github.com/qxnw/lib4go/logger"
+)
+
+//StartTraceServer 启用性能跟踪(协程，内存，堵塞等)
+func StartTraceServer(log *logger.Logger) error {
+	f, err := os.Create("trace.out")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	err = trace.Start(f)
+	if err != nil {
+		return err
+	}
+	defer trace.Stop()
+	addr := "0.0.0.0:19999"
+	log.Info("启用项目跟踪:http://0.0.0.0:19999/debug/pprof/")
+	return http.ListenAndServe(addr, nil)
+}
