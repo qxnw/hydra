be6bdd95c6a7c8019ce934451241006e499fdf86
zkfy <lib4net@163.com>
2018-03-05 16:14:49 +0800

    重构服务引擎

--- quickstart/demo/collector/conf/cacheKey/cacheKey.go
+++
@@ -1,9 +0,0 @@
-package cacheKey
-
-type CKV struct {
-	Key     string
-	Expires int
-}
-var (
-	UserLogin= &CKV{"coupon-system:customer:login:@customerId", 6000}	
-)
--- quickstart/demo/collector/modules/activity/IActivity.go
+++
@@ -1,6 +0,0 @@
-package activity
-
-//IActivity 活动状态
-type IActivity interface {
-	CreateActivity(title string, start string, end string, tag string, url string, amount int, desc string) (id string, err error)
-}
