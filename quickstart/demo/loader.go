6fbff0ad3ef39cce014b102334e9dd7e176a018b
zkfy <lib4net@163.com>
2018-03-06 11:00:11 +0800

    修改用户手册

--- quickstart/demo/loader.go
+++ quickstart/demo/loader.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"github.com/qxnw/helloworld/services/order"
 	"github.com/qxnw/hydra/component"
 	"github.com/qxnw/hydra/engines"
 )
 
 func loader() engines.ServiceLoader {
 	return func(component *component.StandardComponent, container component.IContainer) {
-		component.AddMicroService("/order/query", NewQueryHandler)
+		component.AddMicroService("/order/query", order.NewQueryHandler)
 	}
 }
