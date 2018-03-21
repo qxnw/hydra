07b05bd265d422869c1d4d8d526d921dea7dee52
zkfy <lib4net@163.com>
2018-03-21 17:06:33 +0800

    重构代码

---
+++ quickstart/demo/services/user/login.go
@@ -0,0 +1,28 @@
+package user
+
+import (
+	"github.com/qxnw/hydra/component"
+	"github.com/qxnw/hydra/context"
+)
+
+type LoginHandler struct {
+	container component.IContainer
+	Name      string
+}
+
+func NewLoginHandler(container component.IContainer) (u *LoginHandler) {
+	return &LoginHandler{container: container, Name: "LoginHandler"}
+}
+func (u *LoginHandler) Handle(name string, engine string, service string, ctx *context.Context) (r context.Response, err error) {
+	response := context.GetStandardResponse()
+	//检查用户名密码是否正确
+	response.SetJWTBody(map[string]interface{}{
+		"id": 11000,
+	})
+	response.SetContent(200, "success")
+	return response, nil
+}
+
+func (u *LoginHandler) Close() error {
+	return nil
+}
