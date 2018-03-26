026abceb4cc5b80f79098c253c708a0a4073a2c2
zkfy <lib4net@163.com>
2018-03-26 09:35:24 +0800

    fix:修改govalidator验证

---
+++ examples/simple/loader.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/qxnw/hydra/component"
+	"github.com/qxnw/hydra/engines"
+	"github.com/qxnw/hydra/examples/simple/services/order"
+	"github.com/qxnw/hydra/examples/simple/services/user"
+)
+
+func loader() engines.ServiceLoader {
+	return func(component *component.StandardComponent, container component.IContainer) error {
+		component.AddMicroService("/user/login", user.NewLoginHandler)
+		component.AddMicroService("/order/query", order.NewQueryHandler)
+		return nil
+	}
+}
---
+++ examples/simple/services/order/order.query.go
@@ -0,0 +1,24 @@
+package order
+
+import (
+	"github.com/qxnw/hydra/component"
+	"github.com/qxnw/hydra/context"
+)
+
+type QueryHandler struct {
+	container component.IContainer
+	Name      string
+}
+
+func NewQueryHandler(container component.IContainer) (u *QueryHandler) {
+	return &QueryHandler{container: container, Name: "QueryHandler"}
+}
+func (u *QueryHandler) Handle(name string, engine string, service string, ctx *context.Context) (r context.Response, err error) {
+	response := context.GetObjectResponse()
+	response.SetContent(200, ctx.Request.Ext.GetJWTBody())
+	return response, nil
+}
+
+func (u *QueryHandler) Close() error {
+	return nil
+}
