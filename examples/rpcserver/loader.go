8af41e914dfadef91606b64e0c2d56cfbbce6a14
zkfy <lib4net@163.com>
2018-03-28 15:32:45 +0800

    fix:rpc启动bug

---
+++ examples/rpcserver/loader.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/qxnw/hydra/component"
+	"github.com/qxnw/hydra/engines"
+	"github.com/qxnw/hydra/examples/rpcserver/services/order"
+	"github.com/qxnw/hydra/examples/rpcserver/services/user"
+)
+
+func loader() engines.ServiceLoader {
+	return func(component *component.StandardComponent, container component.IContainer) error {
+		component.AddMicroService("/user/login", user.NewLoginHandler)
+		component.AddMicroService("/order/query", order.NewQueryHandler)
+		component.AddMicroService("/order/bind", order.NewBindHandler)
+		return nil
+	}
+}
