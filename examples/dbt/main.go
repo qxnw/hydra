952bca5b727b46522971d79f12875a4e7022de35
zkfy <lib4net@163.com>
2018-03-28 15:59:58 +0800

    添加dbt

---
+++ examples/dbt/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/qxnw/hydra/engines"
+	"github.com/qxnw/hydra/hydra"
+)
+
+func main() {
+	engines.AddServiceLoader(loader())
+	app := hydra.NewApp(
+		hydra.WithPlatName("hydrav-db"),
+		hydra.WithSystemName("collector"),
+		hydra.WithServerTypes("api"),
+		hydra.WithAutoCreateConf(true),
+		hydra.WithDebug())
+	app.Start()
+}
