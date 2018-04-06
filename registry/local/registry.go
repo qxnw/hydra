ca8391e0e06ec21c387a2aaaeea0d10d08439d06
zkfy <lib4net@163.com>
2018-04-06 08:28:49 +0800

    修改lfs>fs

--- registry/local/registry.go
+++ registry/local/registry.go
@@ -24,5 +24,5 @@ func (z *lkRegistry) Resolve(servers []string, log *logger.Logger) (registry.IRe
 }
 
 func init() {
-	registry.Register("lfs", &lkRegistry{})
+	registry.Register("fs", &lkRegistry{})
 }
