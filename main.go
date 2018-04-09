2f609710c83910aff3c2581b1399ac3b5fdc0e09
zkfy <lib4net@163.com>
2018-04-09 15:38:06 +0800

    .

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ func main() {
 	app := hydra.NewApp(hydra.WithPlatName("hydrav4"),
 		hydra.WithSystemName("collector"),
 		hydra.WithServerTypes("api"),
-		hydra.WithAutoCreateConf(true),
+		hydra.WithAutoCreateConf(),
 		hydra.WithDebug())
 
 	app.Start()
